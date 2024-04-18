5cfa4dc2199d04204e8c3ae844278a146093a20f
Edon Gashi <edongashi2004@outlook.com>
2024-04-18 21:50:39 +0200

    Stage

--- src/main/java/Detyra/SymmetricCaesarEncryption.java
+++ src/main/java/Detyra/SymmetricCaesarEncryption.java
@@ -1,4 +1,23 @@
 package Detyra;
 
-public class SymmetricCaesarEncryption {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class SymmetricCaesarEncryption extends Application {
+    public void start(Stage stage){
+        TextArea plainInput = new TextArea();
+
+
+
+        FlowPane flowPane = new FlowPane();
+        StackPane pane = new StackPane(flowPane);
+        Scene scene = new Scene(pane, 500, 800);
+        Stage stage1 = new Stage();
+        stage1.setScene(scene);
+        stage1.show();
+    }
 }
