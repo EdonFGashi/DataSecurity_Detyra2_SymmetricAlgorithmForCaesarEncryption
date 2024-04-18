b034d5490a38129f9e2acfe4a73456e5931c0c49
Edon Gashi <edongashi2004@outlook.com>
2024-04-18 16:59:52 +0200

    Test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.datasecurity_detyra2_symmetricalgorithmforcaesarencryption {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.datasecurity_detyra2_symmetricalgorithmforcaesarencryption to javafx.fxml;
+    exports com.example.datasecurity_detyra2_symmetricalgorithmforcaesarencryption;
+}
